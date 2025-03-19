7bcfefc33e67fca8c556ade5227975bf6f6b73c7
Shariq Hirani <shariqhirani2010@gmail.com>
2025-03-18 22:08:12 -0700

    added react, updated login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,12 @@ import Dashboard from './pages/Dashboard';
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
